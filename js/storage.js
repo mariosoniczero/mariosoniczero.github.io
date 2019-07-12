a92c9cb0b3d756ff9af44a820cd18b9fa09f5376
mariosoniczero <jmc201204@gmail.com>
2019-07-12 16:36:23 -0400

    Revert "Update storage.js"
    This reverts commit bfe575516202aafe02cdd61d613c5e13c5c205e9.

--- js/storage.js
+++ js/storage.js
@@ -4,8 +4,8 @@ Config.origindomain = 'play.pokemonshowdown.com';
 Config.defaultserver = {
 	id: 'showdown',
 	host: 'https://showdown-zero.herokuapp.com/',
-	port: 80,
-	httpport: 80,
+	port: 443,
+	httpport: 8000,
 	altport: 80,
 	registered: false
 };
