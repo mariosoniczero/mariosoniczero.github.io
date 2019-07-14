174b86fe2e3608c50efd611c61854ad24f71c918
mariosoniczero <jmc201204@gmail.com>
2019-07-14 12:52:00 -0400

    Update battle-dex.js

--- js/battle-dex.js
+++ js/battle-dex.js
@@ -218,7 +218,7 @@ statNamesExceptHP=['atk','def','spa','spd','spe'];this.
 resourcePrefix=function(){
 var prefix='';
 if(!window.document||!document.location||document.location.protocol!=='http:')prefix='https:';
-return prefix+'//play.pokemonshowdown.com/';
+return prefix+'//raw.githubusercontent.com/mariosoniczero/mariosoniczero.github.io/master/';
 }();this.
 
 fxPrefix=function(){
