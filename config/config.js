4fe137800e86004b024f399fb6bb5e9ecf224364
mariosoniczero <jmc201204@gmail.com>
2019-07-14 13:23:40 -0400

    update

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (174b86fe)";
+/* version */ Config.version = "0.11.2 (012bf405)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
