39b5444054fc7dc2bde6b7667a8c74ded2aad5e8
mariosoniczero <jmc201204@gmail.com>
2020-07-17 13:19:41 -0400

    Update sprites

--- sprites/find-missing.pkmn.js
+++ sprites/find-missing.pkmn.js
@@ -11,7 +11,7 @@ const SPECIES = JSON.parse(arr);
 
 const GENS = Object.keys(PokemonSprites.GENS);
 const MISSING = {};
-const LENGTH = 'https://raw.githubusercontent.com/mariosoniczero/mariosoniczero.github.io/master/sprites/'.length;
+const LENGTH = 'https://play.pokemonshowdown.com/sprites/'.length;
 
 function sprites(gen, namestar) {
   const name = namestar.endsWith('*') ? namestar.slice(0, -1) : namestar;
